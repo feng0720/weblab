b02a70eca0084ac42a2e96da4ea0da2f64ec3919
feng0720 <2044391577@qq.com>
2025-08-04 18:19:42 +0800

    react

--- demo.js
+++ demo.js
@@ -1,14 +1,15 @@
-const tem = [19,20,23,25];
-const change = (tem)=>{
-    return tem*9/5+32;
-};
-const modify = (tem,change)=>{
-    for(t of tem){
-        console.log(change(t));
-    }
-};
-modify(tem,change);
-const squre = tem.map((num) => (num*3));
-console.log(squre);
-const the = tem.filter(num=>(num>20));
-console.log(the);
+// const tem = [19,20,23,25];
+// const change = (tem)=>{
+//     return tem*9/5+32;
+// };
+// const modify = (tem,change)=>{
+//     for(t of tem){
+//         console.log(change(t));
+//     }
+// };
+// modify(tem,change);
+// const squre = tem.map((num) => (num*3));
+// console.log(squre);
+// const the = tem.filter(num=>(num>20));
+// console.log(the);
+import React ,{useState} from "react";
