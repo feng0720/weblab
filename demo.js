9081307c1265fbcb68c4a4c36043d9e858a96cf8
feng0720 <2044391577@qq.com>
2025-08-06 11:48:35 +0800

    random color change

--- demo.js
+++ demo.js
@@ -1,15 +1,28 @@
-// const tem = [19,20,23,25];
-// const change = (tem)=>{
-//     return tem*9/5+32;
-// };
-// const modify = (tem,change)=>{
-//     for(t of tem){
-//         console.log(change(t));
-//     }
-// };
-// modify(tem,change);
-// const squre = tem.map((num) => (num*3));
-// console.log(squre);
-// const the = tem.filter(num=>(num>20));
-// console.log(the);
-import React ,{useState} from "react";
+const body = document.querySelector("body");
+const btn = document.querySelector("#changeColor");
+const reset = document.querySelector("#reset");
+const darkColorsArr = [
+    "#2C3E50",
+    "#34495E",
+    "#2C2C2C",
+    "#616A6B",
+    "#4A235A",
+    "#2F4F4F",
+    "#0E4B5A",
+    "#36454F",
+    "#2C3E50",
+    "#800020",
+    ];
+const index = ()=> Math.floor(Math.random()*darkColorsArr.length);
+
+const func = ()=>{
+    body.style.backgroundColor = darkColorsArr[index()];
+}
+const rs = ()=>{
+    if(body.style.backgroundColor !== "antiquewhite"){
+        body.style.backgroundColor = "antiquewhite";
+    }
+}
+btn.onclick = func;
+reset.onclick = rs;
+
