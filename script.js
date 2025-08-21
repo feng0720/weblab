fd0736a83df15f1eda0e3f9a5c3c2aaae24bbae4
feng0720 <2044391577@qq.com>
2025-08-21 11:51:31 +0800

    learn_array

--- script.js
+++ script.js
@@ -1,113 +1,71 @@
-const number = document.getElementById("number");
-const btn = document.getElementById("convert-btn");
-const res = document.getElementById("output");
+const getMean = (array)=>array.reduce((acc,el)=>acc+el,0)/array.length;
 
-const func = ()=>{
-  let v = parseInt(number.value);
-  let nums = [];
-  while(v>0){
-    nums.unshift(v%10);
-    v = Math.floor(v/10);
+const getMedian = (array)=>{
+  const sorted = array.toSorted((a,b)=>a-b);
+  if(sorted.length%2===1){
+    return sorted[Math.floor(sorted.length/2)];
   }
-  let str = "";
-  //function1--forEach--it can not change the array beacause the parameters just a copy
-  // nums.forEach((n,i)=>{
-  //   nums[i]*=10**(nums.length-1-i);
-  // })
-  //function2--map--it can change the array
-  nums = nums.map((n,i)=>n*=10**(nums.length-1-i));
-  // console.log(nums);
-  const len = nums.length;
-  for (let i of nums){
-    if(i>=1000){
-      while(i){
-        str+='M';
-        i-=1000;
-      }
-    }else if(i>=500&&i<1000){
-      if(i===900){
-        str+='CM';
-      }
-      else{
-        str+='D';
-        while((i-500)){
-          str+='C';
-          i-=100;
-        }
-      }
-    }else if(i>=100&&i<500){
-      if(i===400){
-        str+='CD';
-      }
-      else {
-        while(i){
-          str+='C';
-          i-=100;
-        }
-      }
-    }else if(i>=50&&i<100){
-      if(i===90){
-        str+='XC';
-      }else{
-        str+='L';
-        i-=50;
-        while(i){
-          str+='X';
-          i-=10;
-        }
-      }
-    }else if(i>=10&&i<50){
-      if(i===40){
-        str+='XL';
-      }else{
-        while(i){
-          str+='X';
-          i-=10;
-        }
-      }
-    }else if(i>=5&&i<10){
-      if(i===9){
-        str+='IX';
-      }else{
-        str+='V';
-        i-=5;
-        while(i){
-          str+='I';
-          i--;
-        }
-      }
-    }
-    else if(i>=1&&i<5){
-      if(i===4){
-        str+='IV';
-      }else{
-        while(i){
-          str+='I';
-          i--;
-        }
-      }
-    }
+  else {
+    return (sorted[Math.floor(sorted.length/2)-1]+sorted[Math.floor(sorted.length/2)])/2;
   }
-  return str;
 }
 
-const judge = ()=>{
-  if(!number.value){
-    res.innerText = "Please enter a valid number";
+const getMode = (array)=>{
+  const counts = {};
+  //array.forEach(el =>{
+  //  if(counts[el]){
+  //    counts[el]+=1;
+  //  }else{
+  //    counts[el] = 1;
+  //  }
+  //})
+  array.forEach(el=>counts[el] = counts[el]?counts[el]+1:1);
+  if(new Set(Object.values(counts)).size===1){
+    return null;
   }
-  else if(number.value<=0){
-    res.innerText = "Please enter a number greater than or equal to 1";
-  }
-  else if(number.value>=4000){
-    res.innerText = "Please enter a number less than or equal to 3999";
-  }
-  else{
-    res.innerText = func();
-  }
+  //通过Object.keys获得对象中的每个key组成的数组然后对这个数组通过sort进行排序降序排序，然后选择第一个元素就是出现次数最多的那个元素了
+  const highest = Object.keys(counts).sort((a,b)=>counts[b]-counts[a])[0]; 
+  const mode = Object.keys(counts).filter(el=>counts[el]===counts[highest]);
+  return mode.join(", ");
 }
-btn.addEventListener("click",judge)
-number.addEventListener("keydown",(e)=>{
-  if(e.key==="Enter"){
-    judge();
-  }
-})
+
+const getRange = (array)=>{
+  return Math.max(...array)-Math.min(...array);
+}
+
+const getVariance = (array)=>{
+  const mean = getMean(array);
+  const variance = array.reduce((acc,el)=>{
+    const difference = el-mean;
+    const squared = difference**2;
+    return acc+squared;
+  },0)/array.length;
+  return variance;
+}
+
+const getStandardDeviation = (array)=>{
+  const variance = getVariance(array);
+  // const standardDeviation = Math.pow(variance,0.5);
+  const standardDeviation = Math.sqrt(variance);
+  return standardDeviation;
+}
+
+const calculate = ()=>{
+  const value = document.querySelector("#numbers").value;
+  const array = value.split(/,\s*/g);
+  const numbers = array.map(el => {
+    Number(el);
+  }).filter(el=>!isNaN(el));
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
+  const mode = getMode(numbers);
+  const range = getRange(numbers);
+  const variance = getVariance(numbers);
+  const standardDeviation = getStandardDeviation(numbers);
+  document.querySelector("#mean").textContent = mean;
+  document.querySelector("#median").textContent = median;
+  document.querySelector("#mode").textContent = mode;
+  document.querySelector("#range").textContent = range;
+  document.querySelector("#variance").textContent = variance;
+  document.querySelector("#standardDeviation").textContent = standardDeviation;
+}
