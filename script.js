f786ab09a40288b924833cc4c5545d9250b2168d
feng0720 <2044391577@qq.com>
2025-08-22 22:53:06 +0800

    pause

--- script.js
+++ script.js
@@ -24,7 +24,8 @@ const median = (nums)=>{
   return isEven(length)?(nums[Math.ceil(middle)]+nums[Math.ceil(middle+1)])/2:nums[Math.ceil(middle)];
 }
 const spreadsheetFunctions = {
-  sum,average,median
+  sum,average,median,
+  even: nums=>nums.filter(isEven)
 }
 
 const applyFunction = (str)=>{
@@ -32,7 +33,9 @@ const applyFunction = (str)=>{
   const infix = /([\d.]+)([+-])([\d.]+)/;
   const str2 = infixEval(noHigh,infix);
   const functionCall =/([a-z0-9]*)\(([0-9., ]*)\)(?!.*\()/i;
-  const toNumberList = (args)=>args.split(",")
+  const toNumberList = (args)=>args.split(",").map(parseFloat);
+  const apply = (fn,args)=>spreadsheetFunctions[fn.toLowerCase()](toNumberList(args));
+  return str2.replace(functionCall,(match,fn,args)=>spreadsheetFunctions.hasOwnProperty(fn.toLowerCase())?apply(fn,args):match);
 }
 
 const range = (start,end)=>Array(end-start+1).fill(start).map((element,index)=>element+index);
@@ -54,6 +57,8 @@ const evalFormula = (x,cells)=>{
   const rangeExpanded = x.replace(rangeRegex,(_match,char1,num1,char2,num2)=>rangeFromString(num1,num2).map(addCharacters(char1)(char2)));
   const cellRegex = /[A-J][1-9][0-9]?/gi;
   const cellExpanded = rangeExpanded.replace(cellRegex,(match)=>idToText(match.toUpperCase()));
+  const functionExpanded = applyFunction(cellExpanded);
+  return functionExpanded===x?functionExpanded:evalFormula(functionExpanded,cells);
 }
 
 window.onload = ()=>{
@@ -82,5 +87,7 @@ window.onload = ()=>{
 const update = (event)=>{
   const element = event.target;
   const value = element.value.replace(/\s/g,"");
-  if(!value.includes(element.id)&&value[0]==="="){}
+  if(!value.includes(element.id)&&value[0]==="="){
+    element.value = evalFormula(value.slice(1),Array.from(document.getElementById("container").children));
+  }
 }
